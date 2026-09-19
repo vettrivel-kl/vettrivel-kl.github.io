da6c1bb852c204b59931f06ab6222a98d258d376
Vettrivel <vettrivel18@gmail.com>
2026-09-19 22:12:04 +0530

    FAI docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,6 +84,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-1TZBD5LB6Z',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
